4bcc454e4810a8e6c2b0d90b73031aebe52d2c92
Hazhar Maroufi <hazhmar@gmail.com>
2025-07-31 12:37:11 +0330

    some ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,24 @@ function Card(data) {
     <div>
       <ul>
         {data.books.map((book) => (
-          <li key={book.id}>
-            <img src={`/src/assets/${book.image}`} alt="" /> {book.title} -
-            {book.author} - {book.language} - {book.pages} pages -
+          <li key={book.id} className="book-container">
+            <div className="book-data">
+              <div>
+                <img src={`/src/assets/${book.image}`} alt="" />
+              </div>
+              <div className="book_info">
+                <div>
+                  <h2>{book.title}</h2>
+                </div>
+
+                <div> {book.author}</div>
+
+                <div>
+                  {book.language} | {book.pages} pages{" "}
+                </div>
+              </div>
+            </div>
+            <div>like</div>
           </li>
         ))}
       </ul>
