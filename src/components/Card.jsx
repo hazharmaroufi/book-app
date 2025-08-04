12d3e2a704bec36b0e98761807e8683432135476
Hazhar Maroufi <hazhmar@gmail.com>
2025-08-04 19:08:40 +0330

    the fav is working now

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,70 @@
-function Card(data) {
+import { useState } from "react";
+
+function Card(books) {
+  const [favBooks, setFavBooks] = useState([]);
+  const favHandler = (id) => {
+    const theBook = books.books.find((book) => book.id === id);
+    console.log(theBook);
+    setFavBooks((prevFavBooks) => [...prevFavBooks, theBook]);
+
+    console.log(favBooks);
+  };
   return (
-    <div>
-      <ul>
-        {data.books.map((book) => (
-          <li key={book.id} className="book-container">
-            <div className="book-data">
-              <div>
-                <img src={`/src/assets/${book.image}`} alt="" />
-              </div>
-              <div className="book_info">
+    <div className="cardDiv">
+      <div className="main">
+        <ul>
+          {books.books.map((book) => (
+            <li key={book.id} className="book-container">
+              <div className="book-data">
                 <div>
-                  <h2>{book.title}</h2>
+                  <img src={`/src/assets/${book.image}`} alt="" />
                 </div>
+                <div className="book_info">
+                  <div>
+                    <h2>{book.title}</h2>
+                  </div>
 
-                <div> {book.author}</div>
+                  <div> {book.author}</div>
 
-                <div>
-                  {book.language} | {book.pages} pages{" "}
+                  <div>
+                    {book.language} | {book.pages} pages
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>like</div>
-          </li>
-        ))}
-      </ul>
+              <div className="like" onClick={() => favHandler(book.id)}>
+                like
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="sidebar">
+        <ul>
+          {favBooks.map((book) => (
+            <li key={book.id} className="book-container">
+              <div className="book-data">
+                <div>
+                  <img src={`/src/assets/${book.image}`} alt="" />
+                </div>
+                <div className="book_info">
+                  <div>
+                    <h2>{book.title}</h2>
+                  </div>
+
+                  <div> {book.author}</div>
+
+                  <div>
+                    {book.language} | {book.pages} pages
+                  </div>
+                </div>
+              </div>
+              <div className="like" onClick={() => favHandler(book.id)}>
+                like
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
